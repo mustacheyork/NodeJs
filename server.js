8fe5d893eb5a29314ddf24cf4baacc6030b3fbeb
Kanako Kobayashi <mustacheyork@gmail.com>
2014-04-05 01:10:52 +0900

    add server

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var settings = require('./settings.js'); 
+var server = http.createServer();
+server.on('request', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('hello world!');
+    res.end();
+});
+server.listen(settings.port, settings.host);
+console.log("server listening ...");
---
+++ settings.js
@@ -0,0 +1,2 @@
+exports.post = 1337;
+exports.host = '192.168.1.103';
